14f593b1f0d26d84b3cb1972c95c744cdb8b84d7
AthyoP30 <113732351+AthyoP30@users.noreply.github.com>
2024-11-19 18:19:06 -0500

    Update led_code.c

--- src/led_code.c
+++ src/led_code.c
@@ -1,8 +1,15 @@
-void initb() {
+void new_initb() {
   RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-  GPIOB->MODER &= ~ (0x000003F3);
-  GPIOB->PUPDR|= 0x000000A0;
+  GPIOB->MODER |= GPIO_MODER_MODER12_0;
+}
 
-  GPIOB->MODER |= (0x00550000); //the entire hex value is a bit mask
-
+void set_led(int on)
+{
+  if(on){
+    GPIOB->BSRR |= GPIO_BSRR_BS_12;
+  }
+  if(!on)
+  {
+    GPIOB->BSRR |= GPIO_BSRR_BR_12;
+  }
 }
